35086302bdd4acb29a7033f619bc1b9dae23da43
gloryww <73751296+gloryww@users.noreply.github.com>
2024-09-10 14:47:20 +0800

    Add files via upload

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -225,8 +225,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="Kelton's Chatbot"
+        subTitle="Be the Conversation, Lead the Connection."
         logo={<ChatGptIcon />}
       >
         <div className={styles["sidebar-header-bar"]}>
